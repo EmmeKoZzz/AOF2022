fa2b5493b683be55eef271a1ce2201a4b35ec82a
EmmeKoZzz <emme.ko.zzz@gmail.com>
2023-01-03 10:45:46 -0500

    change Point class name to Knot

--- Day 9 Rope Bridge/Knot.cs
+++ Day 9 Rope Bridge/Knot.cs
@@ -1,13 +1,13 @@
 namespace Day_9_Rope_Bridge;
 
-public struct Point
+public struct Knot
 {
    // Attributes
    public int X { get; private set; }
    public int Y { get; private set; }
 
    // Constructor
-   public Point(int x, int y)
+   public Knot(int x, int y)
    {
       X = x;
       Y = y;
@@ -20,16 +20,16 @@ public struct Point
       Y += yStep;
    }
 
-   public bool IsClose(Point p)
+   public bool IsClose(Knot p)
    {
-      if (this.Equals(p)) return true;
+      if (Equals(p)) return true;
       // N,S,E,W,NE,NW,SE,SW
       int[] x = { 0, 0, 1, -1, 1, -1, 1, -1 };
       int[] y = { 1, -1, 0, 0, 1, 1, -1, -1 };
 
       for (int i = 0; i < 8; i++)
       {
-         Point pTemp = p;
+         Knot pTemp = p;
          pTemp.Step(x[i], y[i]);
 
          if (this.Equals(pTemp))
@@ -40,24 +40,24 @@ public struct Point
    }
 
    // Equality Propeties
-   public static bool operator ==(Point p1, Point p2)
+   public static bool operator ==(Knot p1, Knot p2)
    {
       return p1.X == p2.X && p1.Y == p2.Y;
    }
 
-   public static bool operator !=(Point p1, Point p2)
+   public static bool operator !=(Knot p1, Knot p2)
    {
       return !(p1.X == p2.X && p1.Y == p2.Y);
    }
 
-   private bool Equals(Point other)
+   private bool Equals(Knot other)
    {
       return X == other.X && Y == other.Y;
    }
 
    public override bool Equals(object? obj)
    {
-      return obj is Point other && Equals(other);
+      return obj is Knot other && Equals(other);
    }
 
    public override int GetHashCode()
