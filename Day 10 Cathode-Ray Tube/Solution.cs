e93278d7bc3d4e7ff7edd702d4ba7463f8ac6e42
EmmeKoZzz <emme.ko.zzz@gmail.com>
2023-01-04 18:32:29 -0500

    setting a CRT render code

--- Day 10 Cathode-Ray Tube/Solution.cs
+++ Day 10 Cathode-Ray Tube/Solution.cs
@@ -5,17 +5,26 @@ public class CpuProgram
     // ATTRIBUTES
     private int _value = 1;
     private int _cycle;
-    private Dictionary<int, int> _memory = new();
+    private readonly Dictionary<int, int> _memory = new();
+    public string CRT = "";
 
+    // METHODS
     public void SetInstructions(string[] program)
     {
         foreach (var instructionLine in program)
         {
             var instruction = new Instruction(instructionLine);
-
             for (var i = 0; i < instruction.Cycles; i++)
             {
                 _cycle++;
+
+                bool printer = _value == (_cycle + 40) % 40  ||
+                               _value + 1 == (_cycle + 40) % 40  ||
+                               _value - 1 == (_cycle + 40) % 40 ;
+
+                CRT += printer ? "#" : ".";
+                if (_cycle % 40 == 0) CRT += '\n';
+
                 _memory.Add(_cycle, _value);
             }
 
