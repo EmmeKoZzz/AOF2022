0675dc6dc0889e3f4f7b0188d5e8d561f91e5707
EmmeKoZzz <emme.ko.zzz@gmail.com>
2023-01-06 02:22:59 -0500

    Program.cs - 20:26 create Round method   all monkeys play and sum his inspections

--- Day 11 Monkey in the Middle/Program.cs
+++ Day 11 Monkey in the Middle/Program.cs
@@ -6,11 +6,22 @@ class App
     {
         string[] monkeysInfo = new StreamReader("/home/mkz/Coding../AOF2022/Day 11 Monkey in the Middle/input.txt")
             .ReadToEnd().Split("\n\n");
-        
+
         Monkey[] monkeys = new Monkey[monkeysInfo.Length];
-        
+        int[] monkeysInspections = new int[monkeysInfo.Length];
+
         for (int i = 0; i < monkeysInfo.Length; i++)
             monkeys[i] = new Monkey(monkeysInfo[i]);
-        
+
+        for (int i = 0; i < 20; i++)
+            Round(monkeys, monkeysInspections);
+    }
+
+    static void Round(Monkey[] monkeys, int[] monkeysInspections)
+    {
+        for (int i = 0; i < monkeys.Length; i++)
+        {
+            monkeysInspections[i] += monkeys[i].Play(monkeys);
+        }
     }
 }
